bebfc2b0a52f71c7a3a79711bb1c09ed78cabeeb
Dr. John Zoidberg <27244574+MrJohnZoidberg@users.noreply.github.com>
2019-05-16 22:09:13 +0200

    Added multiple answer choices

--- action-Datum_und_Uhrzeit.py
+++ action-Datum_und_Uhrzeit.py
@@ -2,6 +2,7 @@
 
 from hermes_python.hermes import Hermes
 import datetime
+import random
 
 
 USERNAME_INTENTS = "domi"
@@ -33,6 +34,8 @@ def subscribe_intent_callback(hermes, intent_message):
             result_sentence = "Gerade ist es ein Uhr {0} .".format(minutes)
         else:
             result_sentence = "Gerade ist es {0} Uhr {1} .".format(hours, minutes)
+        first_part = ["Gerade ist es", "Es ist jetzt", "Es ist", "Die aktuelle Zeit ist"]
+        result_sentence = random.choice(first_part) + " " + result_sentence
         current_session_id = intent_message.session_id
         hermes.publish_end_session(current_session_id, result_sentence)
 
