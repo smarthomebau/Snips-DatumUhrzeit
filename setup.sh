ccd56a0a82bfc08e94e755fd550b4ae1aa03f910
smarthomebau <info@smarthomebau.de>
2019-11-01 10:46:59 +0100

    rewrite

--- setup.sh
+++ setup.sh
@@ -1,30 +1,26 @@
-#!/usr/bin/env bash
+#/usr/bin/env bash
 set -e
 
-PYTHON=`which python3`
 VENV=venv
 
-if [ -f "$PYTHON" ]
+if [ -d "$VENV" ]
+then
+    rm -r $VENV
+fi
+
+if [ ! -d "$VENV" ]
 then
 
-    if [ ! -d $VENV ]
+    PYTHON=`which python3`
+
+    if [ ! -f $PYTHON ]
     then
-        # Create a virtual environment if it doesn't exist. 
-        $PYTHON -m venv $VENV
-    else
-        if [ -e $VENV/bin/python2 ]
-        then
-            # If a Python2 environment exists, delete it first
-            # before creating a new Python 3 virtual environment.
-            rm -r $VENV
-            $PYTHON -m venv $VENV
-        fi
+        echo "could not find python"
     fi
+    virtualenv -p $PYTHON $VENV
 
-    # Activate the virtual environment and install requirements.
-    . $VENV/bin/activate
-    pip3 install -r requirements.txt
-
-else
-    >&2 echo "Cannot find Python 3. Please install it."
 fi
+
+. $VENV/bin/activate
+
+pip install -r requirements.txt
